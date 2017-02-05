55738de1f874217e1c201bf39c67c4551f94d538
Carlos Andres Rodriguez <cdumar9@hotmail.com>
2017-02-04 22:37:15 -0500

    The models were created

---
+++ GAP.TechnicalTest/GAP.TechnicalTest.API/Models/Store.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GAP.TechnicalTest.API
+{
+	/// <summary>
+	/// Store.
+	/// </summary>
+	public class Store
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:GAP.TechnicalTest.API.Store"/> class.
+		/// </summary>
+		public Store()
+		{
+		}
+
+		#region Properties
+
+		/// <summary>
+		/// Gets or sets the store identifier.
+		/// </summary>
+		/// <value>The store identifier.</value>
+		public int StoreId { get; set; }
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		/// <value>The name.</value>
+		[Required]
+		public string Name { get; set; }
+		/// <summary>
+		/// Gets or sets the address.
+		/// </summary>
+		/// <value>The address.</value>
+		[Required]
+		public string Address { get; set; }
+
+		#endregion
+	}
+}
